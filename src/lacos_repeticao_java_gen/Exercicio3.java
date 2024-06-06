54f62cfa5e081066eb1464b169aabb5dab22a61d
Beazinat <beatrizgarodrigues@gmail.com>
2024-06-06 14:24:56 -0300

    exercícios 3 e 5
    Adicionei a resolução dos exercícios 3 e 5

--- src/lacos_repeticao_java_gen/Exercicio3.java
+++ src/lacos_repeticao_java_gen/Exercicio3.java
@@ -1,10 +1,32 @@
 package lacos_repeticao_java_gen;
 
+import java.util.Scanner;
+
 public class Exercicio3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		int age;
+		int lessThan21 = 0;
+		int moreThan50 = 0;
+		
+		while (true) {
+			System.out.println("Digite uma idade: ");
+			age = scan.nextInt();
+			
+			if (age < 0) {
+				break;
+			}
+			
+			if (age < 21) {
+				lessThan21++;
+			}else if (age > 50) {
+				moreThan50++;
+			}
+		}
+		System.out.println("Total de pessoas menores de 21 anos: " + lessThan21);
+		System.out.println("Total de pessoas maiores de 50 anos: " + moreThan50);
+		
+		scan.close();
 	}
-
 }
--- src/lacos_repeticao_java_gen/Exercicio5.java
+++ src/lacos_repeticao_java_gen/Exercicio5.java
@@ -1,10 +1,24 @@
 package lacos_repeticao_java_gen;
 
+import java.util.Scanner;
+
 public class Exercicio5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scan = new Scanner(System.in);
+		int number;
+		int sum = 0;
+		
+		do {
+			System.out.println("Digite um número inteiro (0 para encerrar): ");
+			number = scan.nextInt();
+			
+			if (number > 0) {
+				sum += number;
+			}
+		}while (number != 0);
+		
+		System.out.println("A soma dos números positivos é: " + sum);
+		scan.close();
 	}
-
 }
